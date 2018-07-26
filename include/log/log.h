a455bfa6b1ce7a2356f82198814c969b769aec3a
钱苏澄 <qiansucheng2017@outlook.com>
2018-07-26 14:58:01 +0800

    add log and state header

---
+++ include/log/log.h
@@ -0,0 +1,12 @@
+#ifndef RAFT_PROJ_LOG_H
+#define RAFT_PROJ_LOG_H
+
+/**
+ * A container of one entry of log information.
+ * */
+namespace SJTU {
+	struct Log { ;
+	};
+};
+
+#endif //RAFT_PROJ_LOG_H
---
+++ include/raft/state.h
@@ -0,0 +1,26 @@
+#ifndef RAFT_PROJ_STATE_H
+#define RAFT_PROJ_STATE_H
+
+#include "../log/log.h"
+
+/**
+ * State is a class of container that stores raft common information inside of it.
+ * */
+namespace SJTU {
+	struct State {
+		/// persistent state on all servers.
+		int currentTerm;
+		int votedFor;
+		Log logs[];
+
+		/// volatile state on all servers.
+		int commitIndex;
+		int lastApplied;
+
+		/// volatile state on leaders.
+		int nextIndex[];
+		int matchIndex[];
+	};
+};
+
+#endif //RAFT_PROJ_STATE_H
