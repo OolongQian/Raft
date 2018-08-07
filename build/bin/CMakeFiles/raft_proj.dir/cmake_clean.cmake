8b85cc3f04caa9d03e72277ff0ac879387b544c2
flypiggg818 <qiansucheng2018@outlook.com>
2018-08-07 16:06:04 +0800

    add some rubbish

---
+++ build/bin/CMakeFiles/raft_proj.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raft_proj.dir/log/log_array.cpp.o"
+  "CMakeFiles/raft_proj.dir/main.cpp.o"
+  "CMakeFiles/raft_proj.dir/my_server.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/identities/candidate.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/identities/follower.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/identities/identity_base.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/identities/leader.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/raft.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/state.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/synchronous_queue/apply_queue.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/synchronous_queue/event_queue.cpp.o"
+  "CMakeFiles/raft_proj.dir/raft/timer/timer.cpp.o"
+  "CMakeFiles/raft_proj.dir/server.cpp.o"
+  "CMakeFiles/raft_proj.dir/server_info.cpp.o"
+  "CMakeFiles/raft_proj.dir/test/test/IdentityTestHelper.cpp.o"
+  "CMakeFiles/raft_proj.dir/test/test/TestCase.cpp.o"
+  "CMakeFiles/raft_proj.dir/hello_proto/hello.grpc.pb.cc.o"
+  "CMakeFiles/raft_proj.dir/hello_proto/hello.pb.cc.o"
+  "CMakeFiles/raft_proj.dir/hello_proto/hello_rpc_client.cc.o"
+  "CMakeFiles/raft_proj.dir/hello_proto/hello_rpc_server.cc.o"
+  "CMakeFiles/raft_proj.dir/raft/raft_proto/raft_client.cc.o"
+  "CMakeFiles/raft_proj.dir/raft/raft_proto/raft_peer.grpc.pb.cc.o"
+  "CMakeFiles/raft_proj.dir/raft/raft_proto/raft_peer.pb.cc.o"
+  "CMakeFiles/raft_proj.dir/raft/raft_proto/raft_server.cc.o"
+  "libraft_proj.pdb"
+  "libraft_proj.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raft_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/bin/CMakeFiles/raft_proj.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libraft_proj.a"
+)
